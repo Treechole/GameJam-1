63e4a6ea528a1954f393e0613403b5b6aa6efc70
Treechole <kanwarchadha.05@gmail.com>
2023-11-08 23:18:12 +0530

    Defined Player Movement

--- GameJam-1/Assets/Scripts/PlayerController.cs
+++ GameJam-1/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    // private Transform position;
+
+    // private void Awake() {
+    //     position = GetComponent<Transform>();
+    // }
+
+    [SerializeField] private float playerSpeed = 5f;
+
+    private void Update() {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement() {
+        Vector2 position = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            position.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            position.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            position.x += 1;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            position.x -= 1;
+        }
+
+        position = position.normalized;
+        transform.position += new Vector3(position.x, position.y ,0) * Time.deltaTime * playerSpeed;
+    }
+
     public int GetTimeManipulation() {
         if (Input.GetKey(KeyCode.Space)) {
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
