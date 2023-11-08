afd4f5ecbc41e8f088d6c5dc5774cef006aafdf6
Treechole <kanwarchadha.05@gmail.com>
2023-11-08 12:22:12 +0530

    Made a proper timer

--- GameJam-1/Assets/Scripts/TimeDisplayer.cs
+++ GameJam-1/Assets/Scripts/TimeDisplayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,7 +6,7 @@ using UnityEngine;
 
 public class TimeDisplayer : MonoBehaviour {
 
-    [SerializeField] private TextMeshProUGUI timer;
+    private TextMeshProUGUI timer;
     private float startTime;
 
     private void Awake() {
@@ -14,8 +15,30 @@ public class TimeDisplayer : MonoBehaviour {
     }
 
     private void Update() {
-        float timePassed = Time.time - startTime;
-        timer.SetText(timePassed.ToString());
+        int timePassed = (int) Mathf.Floor(Time.time - startTime);
+        processTime(timePassed, out int minutes, out int seconds);
+        string text = string.Format("{0}:{1}", addZero(minutes), addZero(seconds));
+        timer.SetText(text);
+    }
+
+    private void processTime(int currentTime, out int minutes, out int seconds) {
+        seconds = currentTime;
+        minutes = 0;
+
+        while (seconds >= 60) {
+            seconds -= 60;
+            minutes += 1;
+        }
+    }
+
+    private string addZero(int time) {
+        string final_time = time.ToString();
+
+        if (time <= 9) {
+            final_time = string.Format("0{0}", time);
+        }
+
+        return final_time;
     }
 
 }
