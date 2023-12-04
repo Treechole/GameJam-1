913387f9fd01d7e1f23a95f6068694059a14af77
Treechole <kanwarchadha.05@gmail.com>
2023-12-04 13:34:58 +0530

    Made shooting system more practical
    Refactored the code so that the bullet spawns at some offset (though temporary) - also now the rigidbodies are player and enemy while the bullet is just a normal collider.
    
    To do - Make collision turn into a trigger to avoid the player being rotated whenever a bullet "collides"

---
+++ GameJam-1/Assets/Scripts/GunController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D charcter) {
+        
+    }
+}
