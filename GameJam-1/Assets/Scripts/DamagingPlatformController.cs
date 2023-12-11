976341648d3a676ad0e2f6fc766ce5b1f5457446
Treechole <kanwarchadha.05@gmail.com>
2023-12-12 01:48:09 +0530

    Added Level Design and to be made"Spikes"

---
+++ GameJam-1/Assets/Scripts/DamagingPlatformController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingPlatformController : MonoBehaviour {
+    private float damagePerBlow = 10f;
+    private float secondsPerBlow = 2f;
+    private bool initiatedDamage = false;
+
+    private void OnTriggerStay2D(Collider2D character) {
+        if (character.gameObject.CompareTag("Player")) {
+            if (!initiatedDamage) {
+                RecursiveDamage(character.gameObject);
+            }
+            initiatedDamage = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D character) {
+        if (character.gameObject.CompareTag("Player")) {
+            initiatedDamage = false;
+        }
+    }
+
+    private void RecursiveDamage(GameObject player) {
+        player.GetComponent<HealthController>().DamageDealt(damagePerBlow);
+        StartCoroutine(RechargeWait(player));
+    }
+
+    private IEnumerator RechargeWait(GameObject player) {
+        yield return new WaitForSeconds(secondsPerBlow);
+        if (initiatedDamage) {
+                RecursiveDamage(player);
+            }
+    }
+}
