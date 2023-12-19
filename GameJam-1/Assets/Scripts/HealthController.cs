e4a2db5e331f464123543df6c18bfaea1b9b9cf2
Treechole <kanwarchadha.05@gmail.com>
2023-12-19 22:15:20 +0530

    kya hi kar skte hain :)

---
+++ GameJam-1/Assets/Scripts/HealthController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+    private float maxHealth;
+    private float health;
+    private GameObject healthBarSprite;
+    private Color fullHealthColor = new Color(0, (float) 0.7058824, 0, 1);
+    private Color halfHealthColor = new Color((float)0.7058824, (float)0.7058824, 0, 1);
+    private Color noHealthColor = new Color((float) 0.7058824, 0, 0, 1);
+
+    private void Awake() {
+        if (this.gameObject.CompareTag("Player")) {
+            maxHealth = GetComponent<PlayerController>().GetMaxHealth();
+        } else if (this.gameObject.CompareTag("Enemy")) {
+            maxHealth = GetComponent<EnemyController>().GetMaxHealth();
+        }
+        health = maxHealth;
+        healthBarSprite = transform.Find("Health Bar/Sprite").gameObject;
+    }
+
+    public void DamageDealt(float damage) {
+        if (health != 0) {
+            health -= damage;
+            FindWithTag(gameObject, "Blood Particle System").GetComponent<BloodGenerator>().BloodSpurt();
+            UpdateHealthBar();
+
+            if (health == 0) {
+                CharacterDied();
+            }
+        }
+    }
+
+    private void UpdateHealthBar () {
+        healthBarSprite.transform.localScale = new Vector3(health/maxHealth, healthBarSprite.transform.localScale.y, healthBarSprite.transform.localScale.z);
+
+        if (health >= maxHealth/2) {
+            healthBarSprite.GetComponent<SpriteRenderer>().color = Color.Lerp(halfHealthColor, fullHealthColor, (2*health/maxHealth) - 1);
+        } else {
+            healthBarSprite.GetComponent<SpriteRenderer>().color = Color.Lerp(noHealthColor, halfHealthColor, 2*health/maxHealth);
+        }
+    }
+
+    private void CharacterDied () {
+        if (this.gameObject.CompareTag("Player")) {
+            Debug.Log("Game Over!");
+        }
+        Destroy(this.gameObject);
+    }
+
+    public GameObject FindWithTag (GameObject parent, string tag) {
+        GameObject requiredChild = null;
+
+        foreach (Transform transform in parent.transform) {
+            if (transform.CompareTag(tag)) {
+                requiredChild = transform.gameObject;
+                break;
+            }
+        }
+
+        return requiredChild;
+    }
+}
